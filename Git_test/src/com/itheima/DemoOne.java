655fdc4a1f6355627d5048f55c682385b01e53d9
LiuJinWang <1217406105@qq.com>
2018-10-29 17:05:46 +0800

    添加demoone.java

---
+++ Git_test/src/com/itheima/DemoOne.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class DemoOne {
+    public static void main(String[] args) {
+        System.out.println("hello  git");
+        System.out.println("hello  git");
+        System.out.println("hello  git");
+        System.out.println("hello  git");
+    }
+}
