dd17cab17e7a464708034af46d8a7232d6ad45dd
LiuJinWang <44538327+LiuJinWang@users.noreply.github.com>
2018-10-29 18:47:30 +0800

    Update Demo3.java
    增加 for循环

--- Git_test/src/com/itheima/Demo3.java
+++ Git_test/src/com/itheima/Demo3.java
@@ -4,6 +4,11 @@ public class Demo3 {
 
     public static void main(String[] args) {
 
-        System.out.println(" Demo3");
+for (int i=1; i<10;i++){
+ System.out.println(" Demo3");
+}
+       
     }
+
+
 }
